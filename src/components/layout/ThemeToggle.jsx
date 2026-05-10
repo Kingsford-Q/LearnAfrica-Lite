e19f9f6411730e0a48fc26f56fba77db8c0570a9
Kingsford Quainoo <Kingsfordquainoo48@gmail.com>
2026-05-10 23:59:24 +0000

    Toggle theme transition

--- src/components/layout/ThemeToggle.jsx
+++ src/components/layout/ThemeToggle.jsx
@@ -7,13 +7,13 @@ export function ThemeToggle() {
   return (
     <div
       onClick={toggleTheme}
-      className="group flex md:h-9 h-14 cursor-pointer items-center md:justify-center rounded-lg bg-card border border-input px-3 shadow-sm transition-all  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring'"
+      className="group flex md:h-9 h-14 cursor-pointer items-center md:justify-center rounded-lg bg-card border border-input px-3 shadow-sm transition-al  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring"
     >
       <div className="ml-4 md:ml-0 flex md:w-full shrink-0 justify-start scale-110">
         {theme === 'light' ? (
-          <Sun className="h-[18px] w-[18px] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Sun className="h-[18px] w-[18px] rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
         ) : (
-          <Moon className="h-[18px] w-[18px] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Moon className="h-[18px] w-[18px] rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
         )}
       </div>
 
